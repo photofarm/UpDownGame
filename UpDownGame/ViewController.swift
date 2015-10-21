27353c7510a26c3a3445eb695b5817d84686a449
photofarm <sdt5@C3-16ui-MacBook-Pro.local>
2015-10-21 17:21:29 +0900

    수정 : 런치 화면

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -10,18 +10,78 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let 최대횟수 = 5
+    var 최대횟수 = 5
     var 시도횟수 = 0
     var 컴퓨터가선택한값 = 0
     var 성공 = false
+    var 타이머 : NSTimer!
+    var 타이머카운트 = 0
     
     @IBOutlet weak var label메시지: UILabel!
     @IBOutlet weak var segment게임선택: UISegmentedControl!
     @IBOutlet weak var progress도전횟수: UIProgressView!
     @IBOutlet weak var label도전횟수: UILabel!
     @IBOutlet weak var text사용자입력: UITextField!
+    @IBOutlet weak var label타이머: UILabel!
 
+    func 도전(사용자 : Int, 컴퓨터 : Int) {
+        시도횟수++
+        
+        label도전횟수.text = String(시도횟수) + "/" + String(최대횟수)
+        progress도전횟수.progress = Float(시도횟수) / Float(최대횟수)
+        타이머카운트 = 0
+
+        if 컴퓨터 == 사용자 {
+            성공 = true
+        } else if 컴퓨터 > 사용자 {
+            label메시지.text = "컴퓨터 숫자보다 작은 값을 입력했습니다."
+        } else {
+            label메시지.text = "컴퓨터 숫자보다 큰 값을 입력했습니다."
+        }
+        
+        text사용자입력.text = ""
+    }
+    
     func 초기화() {
+        let 게임모드 : Int = segment게임선택.selectedSegmentIndex
+        
+        text사용자입력.resignFirstResponder()
+
+        switch 게임모드 {
+        case 0:
+            최대횟수 = 5
+            컴퓨터가선택한값 = (Int)(arc4random() % 10) + 1;
+        case 1:
+            최대횟수 = 10
+            컴퓨터가선택한값 = (Int)(arc4random() % 50) + 1;
+        default:
+            최대횟수 = 15
+            컴퓨터가선택한값 = (Int)(arc4random() % 100) + 1;
+        }
+        
+        시도횟수 = 0
+        label메시지.text = "게임 시작을 눌러주세요."
+        label타이머.text = ""
+        label도전횟수.text = String(시도횟수) + "/" + String(최대횟수)
+        text사용자입력.text = ""
+        progress도전횟수.progress = 0.0
+        
+        print("컴퓨터가선택한값 : \(컴퓨터가선택한값)")
+        
+        if 타이머 != nil {
+            타이머.invalidate()
+        }
+    }
+    
+    func countUP() {
+        label타이머.text = String(타이머카운트)
+        
+        if ++타이머카운트 >= 10 {
+            도전(-1, 컴퓨터: 컴퓨터가선택한값)
+        }
+    }
+    
+    func 게임시작() {
         let 게임모드 : Int = segment게임선택.selectedSegmentIndex
         
         switch 게임모드 {
@@ -36,18 +96,16 @@ class ViewController: UIViewController {
             label메시지.text = "1부터 100사이의 값을 입력해주세요."
         }
         
-        시도횟수 = 0
-        label도전횟수.text = String(시도횟수) + "/" + String(최대횟수)
-        text사용자입력.text = ""
-        progress도전횟수.progress = 0.0
-        
-        print("컴퓨터가선택한값 : \(컴퓨터가선택한값)")
+        let 주기 : NSTimeInterval = 1
+        타이머 = NSTimer.scheduledTimerWithTimeInterval(주기, target: self, selector: "countUP", userInfo: nil, repeats: true)
     }
 
     @IBAction func button확인(sender: UIButton) {
         var 사용자입력 : String = text사용자입력.text!
         var 제목 : String = ""
         var 메시지 : String = ""
+        
+        text사용자입력.resignFirstResponder()
         
         사용자입력 = 사용자입력.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
 
@@ -58,21 +116,7 @@ class ViewController: UIViewController {
         
         let 사용자입력숫자 = Int(사용자입력)
         
-        if 컴퓨터가선택한값 == 사용자입력숫자 {
-            성공 = true
-        } else if 컴퓨터가선택한값 > 사용자입력숫자 {
-            메시지 = "컴퓨터 숫자보다 작은 값을 입력했습니다."
-            label메시지.text = 메시지
-            text사용자입력.text = ""
-        } else {
-            메시지 = "컴퓨터 숫자보다 큰 값을 입력했습니다."
-            label메시지.text = 메시지
-            text사용자입력.text = ""
-        }
-        
-        시도횟수++;
-        label도전횟수.text = String(시도횟수) + "/" + String(최대횟수)
-        progress도전횟수.progress = Float(시도횟수) / Float(최대횟수)
+        도전(사용자입력숫자!, 컴퓨터: 컴퓨터가선택한값)
 
         if 성공 {
             제목 = "확인"
@@ -90,8 +134,6 @@ class ViewController: UIViewController {
             메시지 = "정답 : " + String(컴퓨터가선택한값);
 
             let dialog = UIAlertController(title: 제목, message: 메시지, preferredStyle: UIAlertControllerStyle.Alert)
-//            let cancelAction = UIAlertAction(title: "취소", style: UIAlertActionStyle.Cancel) { (acion) -> Void in print("취소 선택") }
-//            dialog.addAction(cancelAction)
 
             let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default) { (acion) -> Void in print("OK 선택") }
             dialog.addAction(okAction)
@@ -102,6 +144,10 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func button게임시작(sender: UIButton) {
+        게임시작()
+    }
+    
     @IBAction func segment게임모드변경(sender: UISegmentedControl) {
         초기화()
     }
@@ -109,13 +155,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-//        label메시지.text = "1부터 10사이의 값을 입력해주세요."
-//        
-//        컴퓨터가선택한값 = (Int)(arc4random() % 5) + 1;
-//        label도전횟수.text = String(시도횟수) + "/" + String(최대횟수)
-//        progress도전횟수.progress = 0.0
-//
-//        print("컴퓨터가선택한값 : \(컴퓨터가선택한값)")
+
         초기화()
     }
 
